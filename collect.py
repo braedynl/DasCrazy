819024951208f1a02a62acfe39384a5ca2861a2e
letting4 <letting4@msu.edu>
2021-06-19 12:02:24 -0400

    Some clean-up and speed improvements

--- collect.py
+++ collect.py
@@ -17,10 +17,11 @@ BROADCASTER_ID = 207813352
 SERVER = "irc.chat.twitch.tv"
 PORT = 6667
 REQUEST_URL = f"https://api.twitch.tv/helix/channels?broadcaster_id={BROADCASTER_ID}"
-REQUEST_HEADERS = {"Client-Id": CLIENT_ID, "Authorization": f"Bearer {BEARER_TOKEN}"}
+REQUEST_HEADERS = {"Client-Id": CLIENT_ID,
+                   "Authorization": f"Bearer {BEARER_TOKEN}"}
 
 # Response-parsing pattern
-SEARCH_PATTERN = re.compile(r":(.*)!.* PRIVMSG #%s :(.*)" % BROADCASTER_LOGIN)
+SEARCH_PATTERN = re.compile(":(.*)!.* PRIVMSG #%s :(.*)" % BROADCASTER_LOGIN)
 
 
 def get_metadata() -> Tuple[str, str]:
@@ -46,36 +47,67 @@ def main():
     sock = socket()
     sock.connect((SERVER, PORT))
 
-    sock.send(f"PASS {IRC_TOKEN}\n".encode("utf-8"))
+    sock.send(f"PASS oauth:{IRC_TOKEN}\n".encode("utf-8"))
     sock.send(f"NICK {PERSONAL_LOGIN}\n".encode("utf-8"))
     sock.send(f"JOIN #{BROADCASTER_LOGIN}\n".encode("utf-8"))
+
+    resp = sock.recv(1024).decode("utf-8")
+    if "Welcome" not in resp:
+        print("Connection to Twitch IRC failed, response:")
+        print(resp)
+        return
 
     print("Connection to Twitch IRC successful, entering main loop...")
     print("Use CTRL + C to stop at anytime\n")
 
+    row_temp = {"sent": '', "game_name": '',
+                "title": '', "user": '', "message": ''}
+
     try:
         while True:
             resp = sock.recv(1024).decode("utf-8", errors='ignore')
+
             if resp.startswith("PING"):
                 sock.send("PONG\n".encode("utf-8"))
+                print(f"Pong issued at {datetime.now()}")
                 continue
 
             match = SEARCH_PATTERN.search(resp)
+
             if match is None:
                 continue
+
+            # A blanket search for just "peepoHas" is done here to save
+            # time before another recv() call; further classification
+            # to find if it's truly a "das crazy" moment is done in post
 
             user, message = match.groups()
 
             if "peepoHas" in message:
                 sent = datetime.now()
                 game_name, title = get_metadata()
-                df = df.append({"sent": sent, "game_name": game_name, "title": title, "user": user,
-                                "message": message.replace('\n', ' ').replace('\r', '')}, ignore_index=True)
+
+                # It's slightly faster to declare a dictionary first, and set its values later
+                # as opposed to initializating a dictionary with values
+                row_temp["sent"] = sent
+                row_temp["game_name"] = game_name
+                row_temp["title"] = title
+                row_temp["user"] = user
+                row_temp["message"] = message.replace('\n', ' ').replace('\r', '')
+
+                df = df.append(row_temp, ignore_index=True)
+
                 print(f"peepoHas found at {sent}")
 
+                # Did you know two chained replace() calls is faster than
+                # any other method to replace two substrings? Crazy...
+
     except KeyboardInterrupt:
-        print("Exporting...")
+        print("Closing socket and exporting data...")
+
+        sock.close()
         df.to_csv('data/real.csv', index=False)
+
         print("Done.")
 
 
